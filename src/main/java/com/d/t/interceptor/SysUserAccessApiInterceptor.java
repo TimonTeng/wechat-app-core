32e66b4b57f3e0cc54163dc032c2046d39e47920
d.penghai <d.penghai@gmail.com>
2016-11-23 17:39:03 +0800

    111

--- src/main/java/com/d/t/interceptor/SysUserAccessApiInterceptor.java
+++ src/main/java/com/d/t/interceptor/SysUserAccessApiInterceptor.java
@@ -5,37 +5,40 @@ package com.d.t.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class SysUserAccessApiInterceptor extends HandlerInterceptorAdapter {
 	
-//	private final Logger log = LogManager.getLogger(getClass());
+	private final Logger log = LogManager.getLogger(getClass());
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println(" SysUserAccessApiInterceptor.preHandle ");
+		log.info(" SysUserAccessApiInterceptor.preHandle ");
 		return super.preHandle(request, response, handler);
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		System.out.println(" SysUserAccessApiInterceptor.postHandle ");
+		log.info(" SysUserAccessApiInterceptor.postHandle ");
 		super.postHandle(request, response, handler, modelAndView);
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		System.out.println(" SysUserAccessApiInterceptor.afterCompletion ");
+		log.info(" SysUserAccessApiInterceptor.afterCompletion ");
 		super.afterCompletion(request, response, handler, ex);
 	}
 
 	@Override
 	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println(" SysUserAccessApiInterceptor.afterConcurrentHandlingStarted ");
+		log.info(" SysUserAccessApiInterceptor.afterConcurrentHandlingStarted ");
 		super.afterConcurrentHandlingStarted(request, response, handler);
 	}
  
